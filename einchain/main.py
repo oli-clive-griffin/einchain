fb847cf0e2c97f21efd1c2c37cbdb6f3e08ff3ad
Oli Clive-Griffin <o.clivegriffin@gmail.com>
2024-05-16 18:16:27 +1200

    pack

--- einchain/main.py
+++ einchain/main.py
@@ -3,6 +3,8 @@ from torch import Tensor
 import torch
 import typing
 
+Shape = tuple[int, ...] | list[int]
+
 class EinChain:
     x: Tensor
     current_pattern: str
@@ -15,44 +17,48 @@ class EinChain:
         self.x = x
         self.current_pattern = pattern
 
-    @typing.overload
-    def einsum(self, tensor: Tensor, target_pattern: str, /) -> 'EinChain': ...
-    @typing.overload
-    def einsum(self, tensor1: Tensor, tensor2: Tensor, target_pattern: str, /) -> 'EinChain': ...
-    @typing.overload
-    def einsum(self, tensor1: Tensor, tensor2: Tensor, tensor3: Tensor, target_pattern: str, /) -> 'EinChain': ...
-    @typing.overload
-    def einsum(self, tensor1: Tensor, tensor2: Tensor, tensor3: Tensor, tensor4: Tensor, target_pattern: str, /) -> 'EinChain': ...
-    def einsum(self, *target_tensors_and_pattern: Tensor | str) -> 'EinChain':
-        target_pattern = target_tensors_and_pattern[-1]
-        tensors = [self.x] + list(target_tensors_and_pattern[:-1])
-        pattern, new_current_pattern = self.transformation_to(target_pattern)
-        res = einops.einsum(*tensors, pattern)
-        return EinChain(res, new_current_pattern)
+    def einsum(self, pattern: str, *given_tensors: Tensor) -> 'EinChain':
+        # pattern = tensors_and_target_pattern[-1]
+        tensors = [self.x] + list(given_tensors) # list(tensors_and_target_pattern[:-1])
+        res = einops.einsum(*tensors, self._transformation_with_self(pattern))
+        return EinChain(res, self._end_pattern(pattern))
+
+    def pack(self, pattern, *given_tensors: Tensor) -> 'EinChain':
+        tensors = [self.x] + list(given_tensors)
+        res, ps = einops.pack(tensors, pattern)
+        new_pattern = self._end_pattern(pattern)
+        return EinChain(res, "")
+    
+    def unpack(self, packed_shapes: list[Shape], target_pattern: str) -> 'EinChain':
+        pattern = self._transformation_to(target_pattern)
+        res = einops.unpack(self.x, packed_shapes, pattern)
+        return EinChain(res, self._end_pattern(target_pattern))
 
     def rearrange(self, target_pattern: str, **axes_lengths: int) -> 'EinChain':
-        pattern, new_current_pattern = self.transformation_to(target_pattern)
+        pattern = self._transformation_to(target_pattern)
         res = einops.rearrange(self.x, pattern, **axes_lengths)
-        return EinChain(res, new_current_pattern)
+        return EinChain(res, self._end_pattern(target_pattern))
 
     def reduce(self, target_pattern: str, reduction: str, **axes_lengths: int) -> 'EinChain':
-        pattern, new_current_pattern = self.transformation_to(target_pattern)
+        pattern = self._transformation_to(target_pattern)
         res = einops.reduce(self.x, pattern, reduction, **axes_lengths)
-        return EinChain(res, new_current_pattern)
+        return EinChain(res, self._end_pattern(target_pattern))
 
     def repeat(self, target_pattern: str, **axes_lengths: int) -> 'EinChain':
-        pattern, new_current_pattern = self.transformation_to(target_pattern)
+        pattern = self._transformation_to(target_pattern)
         res = einops.repeat(self.x, pattern, **axes_lengths)
-        return EinChain(res, new_current_pattern)
+        return EinChain(res, self._end_pattern(target_pattern))
 
-    def transformation_to(self, target_pattern: str) -> tuple[str, str]:
-        end_pattern = target_pattern.split('->')[-1].strip()
+    def _end_pattern(self, target_pattern: str) -> str:
+        return target_pattern.split('->')[-1].strip()
 
-        if target_pattern.strip().startswith(','):
-            return f"{self.current_pattern}{target_pattern}", end_pattern
-        if target_pattern.strip().startswith('->'):
-            return f"{self.current_pattern} {target_pattern}", end_pattern
-        raise ValueError()
+    def _transformation_with_self(self, pattern: str) -> str:
+        return pattern.replace('self', self.current_pattern)
+
+    def _transformation_to(self, target_pattern: str) -> str:
+        if not target_pattern.strip().startswith('->'):
+            raise ValueError()
+        return f"{self.current_pattern} {target_pattern}"
 
     def tensor(self) -> Tensor:
         return self.x
@@ -67,18 +73,15 @@ class EinChain:
         ]
         return ', '.join(dims)
 
-
 if __name__ == "__main__":
     bchw = torch.randn(2, 3, 4, 5)
     hwc = torch.randn(4, 5, 3)
 
-    t = EinChain(bchw, 'b c h w')\
-        .rearrange("-> b h w c")\
-        .einsum(hwc, ", h w c -> b h w")\
-        .rearrange("-> b h w 1")\
-        .repeat("-> b h w 3")\
-    
+    t = (EinChain(bchw, 'b c h w')
+        .rearrange("-> b h w c")
+        .einsum("self, h w c -> b h w", hwc)
+        .rearrange("-> b h w 1")
+        .repeat("-> b h w 3")
+        .pack("b h w *", torch.randn(2, 4, 5)))
+
     print(t)
-    
-
-
